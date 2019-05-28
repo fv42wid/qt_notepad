7dfad1fcdcec5f19fa70c5540056f71e8d17ca42
Frank Vinci <fv42wid@yahoo.com>
2019-05-28 15:16:38 -0400

    add functions to notepadd.cpp, keep going

--- notepad.cpp
+++ notepad.cpp
@@ -6,9 +6,81 @@ Notepad::Notepad(QWidget *parent) :
     ui(new Ui::Notepad)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+
+    connect(ui->actionNew, &QAction::triggered, this &Notepad::newDocument);
+    connect(ui->actionOpen, &QAction::triggered, this, &Notepad::open);
+    connect(ui->actionSave, &QAction::triggered, this, &Notepad::save);
+    connect(ui->actionSave_as, &QAction::triggered, this, &Notepad::saveAs);
+    connect(ui->actionPrint, &QAction::triggered, this, &Notepad::print);
+    connect(ui->actionExit, &QAction::triggered, this, &Notepad::exit);
+    connect(ui->actionCopy, &QAction::triggered, this, &Notepad::copy);
+    connect(ui->actionCut, &QAction::triggered, this, &Notepad::cut);
+    connect(ui->actionPaste, &QAction::triggered, this, &Notepad::paste);
+    connect(ui->actionUndo, &QAction::triggered, this, &Notepad::undo);
+    connect(ui->actionRedo, &QAction::triggered, this, &Notepad::redo);
+    connect(ui->actionFont, &QAction::triggered, this, &Notepad::selectFont);
+    connect(ui->actionBold, &QAction::triggered, this, &Notepad::setFontBold);
+    connect(ui->actionUnderline, &QAction::triggered, this, &Notepad::setFontUnderline);
+    connect(ui->actionItalic, &QAction::triggered, this, &Notepad::setFontItalic);
+    connect(ui->actionAbout, &QAction::triggered, this, &Notepadd::about);
+
+    // disable menu actions for unavailable features
+#if !QT_CONFIG(printer)
+    ui->actionPrint->setEnabled(false);
+#endif
+
+#if !QT_CONFIG(clipboard)
+    ui->actionCut->setEnabled(false);
+    ui->actionCopy->setEnabled(false);
+    ui->actionPaste->setEnabled(false);
+#endif
 }
 
 Notepad::~Notepad()
 {
     delete ui;
 }
+
+void Notepad::newDocument() {
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+void Notepad::open() {
+    QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(fileName);
+    currentFile = fileName;
+    if(!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+void Notepad::save() {
+    QString fileName;
+    // if we don't have a filename from before, get one
+    if(currentFile.isEmpty()) {
+        fileName = QFileDialog::getSaveFileName(this, "Save");
+        currentFile = fileName;
+    } else {
+        fileName = currentFile;
+    }
+
+    QFile file(fileName);
+    if(!file.open(QIODevice::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return
+    }
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
